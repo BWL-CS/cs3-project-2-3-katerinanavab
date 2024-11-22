00c34142fff6eb029408628fd9a2416d75e35183
Katerina Navab <94501766+katerinanavab@users.noreply.github.com>
2024-11-22 15:32:45 +0000

    finished

--- main.py
+++ main.py
@@ -53,18 +53,27 @@ def draw_cat_plot():
 # Function to draw Heat Map
 def draw_heat_map():
   # Clean the data
-  df_heat = None
+  # Leave out patients where their diastolic (resting) pressure is higher than their systolic pressure (incorrect medically)
+  df_heat = df[df['ap_lo'] <= df['ap_hi']]
+  # Leave out "extreme values" by using percentiles
+  # we check the [condition], identifying rows that are greater than the 2.5% percentile of the height data, then pull rows that meet the condition from df_heat
+  # therefore, leaving out the too-small values
+  df_heat = df_heat[df['height'] >= df['height'].quantile(0.025)]
+  df_heat = df_heat[df['height'] <= df['height'].quantile(0.975)]
+  df_heat = df_heat[df['weight'] >= df['weight'].quantile(0.025)]
+  df_heat = df_heat[df['weight'] <= df['weight'].quantile(0.975)]
 
   # Calculate the correlation matrix
-  corr = None
+  corr = df_heat.corr()
 
   # Generate a mask for the upper triangle
-  mask = None
+  mask = np.triu(corr)
 
   # Set up the matplotlib figure
-  fig, ax = None
+  fig, ax = plt.subplots(figsize=(16,16))
 
   # Draw the heatmap with 'sns.heatmap()'
+  sns.heatmap(corr, mask=mask, annot=True, fmt='.1f')
 
   # Do not modify the next two lines
   fig.savefig('heatmap.png')
